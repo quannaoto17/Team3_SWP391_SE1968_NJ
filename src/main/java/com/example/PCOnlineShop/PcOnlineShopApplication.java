45a4788c414a504981d7ab5e3cc0d16d15df665e
Naoto <quannaoto17@gmail.com>
2025-09-27 22:31:21 +0700

    no message

---
+++ src/main/java/com/example/PCOnlineShop/PcOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.PCOnlineShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcOnlineShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PCOnlineShop/PcOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PCOnlineShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
