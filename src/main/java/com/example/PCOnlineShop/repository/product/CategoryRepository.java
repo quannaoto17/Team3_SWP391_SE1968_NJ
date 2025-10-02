79da2e3c150886be4d3884047059996899b67481
minh51220033 <minhnro72@gmail.com>
2025-10-03 01:55:14 +0700

    Update

--- src/main/java/com/example/PCOnlineShop/repository/product/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.PCOnlineShop.repository.product;
-
-import com.example.PCOnlineShop.model.product.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
