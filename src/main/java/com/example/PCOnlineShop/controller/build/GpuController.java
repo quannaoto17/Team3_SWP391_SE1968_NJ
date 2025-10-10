4f7eef4a1eb8651338039c40904af2eb70aaad23
Nguyễn Anh Quân <quannaoto17@gmail.com>
2025-10-10 23:29:33 +0700

    Sửa dto và session của build để mở rộng (#24)

--- src/main/java/com/example/PCOnlineShop/controller/build/GpuController.java
+++ src/main/java/com/example/PCOnlineShop/controller/build/GpuController.java
@@ -1,7 +1,9 @@
 package com.example.PCOnlineShop.controller.build;
 
-import com.example.PCOnlineShop.dto.build.BuildItemId;
+
+import com.example.PCOnlineShop.dto.build.BuildItemDto;
 import com.example.PCOnlineShop.service.build.BuildService;
+import com.example.PCOnlineShop.service.build.GpuService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,19 +16,20 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class GpuController {
     private final BuildService buildService;
+    private final GpuService gpuService;
 
     // Hien thi danh sach GPU
     @GetMapping("/gpu")
-    public String showGpuPage(@ModelAttribute("buildItems") BuildItemId buildItemId, Model model) {
-        model.addAttribute("gpus", buildService.getCompatibleGPUs(buildItemId));
+    public String showGpuPage(@ModelAttribute("buildItems") BuildItemDto buildItem, Model model) {
+        model.addAttribute("gpus", buildService.getCompatibleGPUs(buildItem));
         return "/build/build-gpu";
     }
 
     // Chon GPU
     @PostMapping("/selectGpu")
     public String selectGpu(@RequestParam int gpuId,
-                            @ModelAttribute("buildItems") BuildItemId buildItemId) {
-        buildItemId.setGpuId(gpuId);
+                            @ModelAttribute("buildItems") BuildItemDto buildItem) {
+        buildItem.setGpu(gpuService.getGpuById(gpuId));
         return "redirect:/build/case";
     }
 }
