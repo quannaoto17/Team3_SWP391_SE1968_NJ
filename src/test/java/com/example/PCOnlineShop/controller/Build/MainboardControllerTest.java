e3ab5e9374d01eba0ac5140bb6e9c799384a1025
Naoto <quannaoto17@gmail.com>
2025-10-12 20:49:52 +0700

    Update .gitignore to exclude target directory

--- src/test/java/com/example/PCOnlineShop/controller/Build/MainboardControllerTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.PCOnlineShop.controller.Build;
-
-import com.example.PCOnlineShop.controller.build.MainboardController;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest(MainboardController.class)
-public class MainboardControllerTest {
-}
