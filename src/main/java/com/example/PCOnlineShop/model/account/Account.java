94bed3afc502bd59d7fdad180a16d40062f5e724
minh51220033 <minhnro72@gmail.com>
2025-10-14 22:21:16 +0700

    Update validate add,edit Staff (#38)

--- src/main/java/com/example/PCOnlineShop/model/account/Account.java
+++ src/main/java/com/example/PCOnlineShop/model/account/Account.java
@@ -2,7 +2,9 @@ package com.example.PCOnlineShop.model.account;
 
 import com.example.PCOnlineShop.constant.RoleName;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
+
 
 @Entity
 @Table(name = "account")
@@ -16,9 +18,13 @@ public class Account {
     @Column(name = "account_id")
     private int accountId;
 
+    @NotBlank(message = "Phone number is required")
+    @Size(min = 9, max = 15, message = "Phone number must be between 9 and 15 digits")
     @Column(name = "phone_number")
     private String phoneNumber;
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     @Column(name = "password", nullable = false, length = 255)
     private String password;
 
@@ -26,21 +32,25 @@ public class Account {
     @Column(name = "role", length = 50)
     private RoleName role;
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     @Column(name = "email", length = 100)
     private String email;
 
+    @NotBlank(message = "First name is required")
     @Column(name = "first_name")
     private String firstname;
 
+    @NotBlank(message = "Last name is required")
     @Column(name = "last_name")
     private String lastname;
 
     @Column(name = "gender")
     private Boolean gender;
 
+    @NotBlank(message = "Address is required")
     @Column(name = "address", length = 255)
     private String address;
-
 
     @Column(name = "enabled")
     private Boolean enabled = true;
