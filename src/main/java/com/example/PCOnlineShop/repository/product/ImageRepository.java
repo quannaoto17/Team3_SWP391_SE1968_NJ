e58b442e7401e8811c1a19a11499d26078972555
Nguyễn Minh Trí <trinmhe189020@fpt.edu.vn>
2025-10-21 22:24:28 +0700

    chỉnh sửa lại tính năng update sản phẩm và hiển thị hình ảnh (#67)

--- src/main/java/com/example/PCOnlineShop/repository/product/ImageRepository.java
+++ src/main/java/com/example/PCOnlineShop/repository/product/ImageRepository.java
@@ -5,7 +5,9 @@ import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImageRepository extends JpaRepository <Image,Integer> {
-
+    List<Image> findByProduct_ProductId(int productId);
 }
