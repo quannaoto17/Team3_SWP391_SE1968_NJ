69ed52d78047fbb53d3dc9fb5379c9517a989be7
DESKTOP-HA51UR4\pda12 <anhpdhe180893@fpt.edu.vn>
2025-11-11 21:34:33 +0700

    fix(register)

--- src/main/java/com/example/PCOnlineShop/PcOnlineShopApplication.java
+++ src/main/java/com/example/PCOnlineShop/PcOnlineShopApplication.java
@@ -9,11 +9,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class PcOnlineShopApplication {
 
     public static void main(String[] args) {
