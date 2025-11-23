7636aa544ef0632f6d19c752556c6ecc372d5bfc
duyanh112 <147604527+duyanh112@users.noreply.github.com>
2025-11-24 01:10:16 +0700

    Auth6 (#175)
    * fix(header): fix header customer
    
    * fix nhanh
    
    * hot fix

--- src/main/java/com/example/PCOnlineShop/config/auth/SecurityConfig.java
+++ src/main/java/com/example/PCOnlineShop/config/auth/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
                         .requestMatchers("/orders/list", "/orders/detail/**").hasAnyRole("CUSTOMER", "STAFF", "ADMIN")
                         .requestMatchers("/cart/**").hasRole("CUSTOMER") // Only Customers can access the cart
                         .requestMatchers("/checkout/**", "/payment/**", "/cart/**").hasRole("CUSTOMER")
-                        .requestMatchers("/profile/**").hasRole("CUSTOMER")
+                        .requestMatchers("/profile/**").hasAnyRole("CUSTOMER", "STAFF")
                         // --- Đường dẫn cập nhật chỉ cho Staff/Admin ---
                         .requestMatchers("/orders/update-all-status").hasAnyRole("STAFF", "ADMIN")
                         .requestMatchers("/customer/list/**", "/customer/add/**", "/customer/view/**").hasAnyRole("ADMIN", "STAFF")
